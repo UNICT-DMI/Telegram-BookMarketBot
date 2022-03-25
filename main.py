0caeb402ceb14c433fd30f7435cc95c629720538
ellegint <lucagarro98@gmail.com>
2022-03-25 13:03:30 +0100

    minor refactoring

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import logging
 from module.handlers import handlers
 
 def main() -> None:
-    TOKEN = '1739559917:AAEg2DwAEB2k5CmFUv4T1HuqBh2E3-Sj6go'
+    TOKEN = 'TOKEN'
     updater= Updater(TOKEN, use_context=True)
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
     handlers(updater)
