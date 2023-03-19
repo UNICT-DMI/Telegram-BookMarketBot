a2c5445101031bade04c7602600b4737aa0cce51
Salvo Polizzi <plzslv03a25c351k@studium.unict.it>
2023-03-19 09:58:40 +0100

    fix: solve pylint errors

--- module/add_book.py
+++ module/add_book.py
@@ -3,6 +3,16 @@ from module.shared import INSERT
 from telegram.ext import CallbackContext
 
 def add_book(context: CallbackContext, chat_id: int, isbn: str, title: str, authors: str) -> None:
+    """called in the request of a book or in selling books
+        Add a book in the Books table
+
+        Args:
+            context: context passed by the handler
+            chat_id: id of the chat of the message
+            isbn: isbn of the book to add
+            title: title of the book to add
+            authors: authors of the book to add
+    """
     query = "INSERT INTO Books(ISBN, Title, Authors) VALUES(?,?,?)"
     params = (isbn, title, authors)
     connect_and_execute(context, chat_id, query, params, INSERT)
--- module/stats.py
+++ module/stats.py
@@ -4,6 +4,7 @@ from telegram.ext import CallbackContext
 from module.shared import SELECT, STATS_MESSAGE, DB_ERROR
 from module.create_connection import connect_and_execute
 
+
 def stats(update: Update, context: CallbackContext) -> None:
     """called by /stats
         It returns the number of books that are now on sale
@@ -14,11 +15,12 @@ def stats(update: Update, context: CallbackContext) -> None:
 
     query = "SELECT COUNT(*) FROM Market"
     chat_id = update.effective_chat.id
-    
+
     try:
         num: int = connect_and_execute(
             context=context, chat_id=chat_id, query=query, params=(), operation=SELECT
         )[0][0]
         context.bot.send_message(chat_id=chat_id, text=STATS_MESSAGE+str(num))
-    except:
+    except ValueError as v:
+        print(str(v))
         context.bot.send_message(chat_id=chat_id, text=DB_ERROR)
