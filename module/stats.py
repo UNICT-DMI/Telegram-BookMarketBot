4e506ecc249d5102ed9320a31dbc8d08d97fa103
Salvo Polizzi <plzslv03a25c351k@studium.unict.it>
2023-03-19 09:49:55 +0100

    fix: solve problems with value of num

--- module/stats.py
+++ module/stats.py
@@ -1,7 +1,7 @@
 """/stats command"""
 from telegram import Update
 from telegram.ext import CallbackContext
-from module.shared import SELECT, STATS_MESSAGE
+from module.shared import SELECT, STATS_MESSAGE, DB_ERROR
 from module.create_connection import connect_and_execute
 
 def stats(update: Update, context: CallbackContext) -> None:
@@ -12,11 +12,13 @@ def stats(update: Update, context: CallbackContext) -> None:
             update: update event
             context: context passed by the handler"""
 
-    query: str = "SELECT COUNT(*) FROM Market"
-    chat_id: int = update.effective_chat.id
-    params = ()
-    num: int = connect_and_execute(
-        context=context, chat_id=chat_id, query=query, params=params, operation=SELECT
-    )[0][0]
-
-    context.bot.send_message(chat_id=chat_id, text=STATS_MESSAGE+str(num))
+    query = "SELECT COUNT(*) FROM Market"
+    chat_id = update.effective_chat.id
+    
+    try:
+        num: int = connect_and_execute(
+            context=context, chat_id=chat_id, query=query, params=(), operation=SELECT
+        )[0][0]
+        context.bot.send_message(chat_id=chat_id, text=STATS_MESSAGE+str(num))
+    except:
+        context.bot.send_message(chat_id=chat_id, text=DB_ERROR)
